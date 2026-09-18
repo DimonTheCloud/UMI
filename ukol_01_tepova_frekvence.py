002d3e06a4f2f2e248d153c5ca178cdf163254a4
Dmytro Antonenko <dimchik41414@gmail.com>
2026-09-18 09:36:48 +0200

    test

--- ukol_01_tepova_frekvence.py
+++ ukol_01_tepova_frekvence.py
@@ -13,14 +13,14 @@ TODO a spustte (podrobne kroky viz prezentace ke cviceni).
 tepova_frekvence = [58, 72, 101, 65, 88, 110, 45, 76, 99, 130, 60, 100]
 
 # TODO 1: vase predikce - kolik hodnot bude mimo rozsah 60-100 (vcetne)?
-moje_predikce = None  # nahradte cislem, napr. moje_predikce = 4
+moje_predikce = 4
 
 # TODO 2: doplnte inicializaci promenne
-mimo_rozsah = ...
+mimo_rozsah = []
 
 for hodnota in tepova_frekvence:
     # TODO 3: doplnte podminku - hodnota je MIMO klidovy rozsah 60-100 (vcetne)
-    if ...:
+    if 60 <= hodnota <= 100:
         mimo_rozsah.append(hodnota)
 
 print(f"Vase predikce: {moje_predikce}")
@@ -29,5 +29,5 @@ print("Hodnoty mimo rozsah:", mimo_rozsah)
 
 # TODO 4: doplnte, kolik procent pacientu ma hodnotu mimo rozsah
 # a vypiste zaokrouhlene na 1 desetinne misto.
-podil = ...
+podil = len(mimo_rozsah) * 100/len(tepova_frekvence)
 print(f"Podil mimo rozsah: {podil:.1f} %")
