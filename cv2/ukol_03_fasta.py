d528b56f2da294e67595cd131ba997a36a455855
Dmytro Antonenko <dimchik41414@gmail.com>
2026-09-25 10:19:21 +0200

    1

--- cv2/ukol_03_fasta.py
+++ cv2/ukol_03_fasta.py
@@ -9,17 +9,20 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
 # TODO 1: nactete FASTA soubor, ktery jste stahli z NCBI
-record = open("streptococcus.fasta", "rU")
+record = SeqIO.read("streptocok.fasta", format="fasta")
 
 # TODO 2: vypiste jednotlive atributy - id, popis (.description) a delku sekvence
-attribute_id =
+print(record.id)
+print(record.description)
+print(len(record.seq))
 
 # TODO 3: vyindexujte prvnich 50 nukleotidu sekvence do promenne
 # "novy_uryvek"
-novy_uryvek =
+novy_uryvek = record.seq[0:50]
+print(novy_uryvek)
 
 # TODO 4: vytvorte novy SeqRecord z "novy_uryvek" s id "Streptococcus_50NT"
-novy_record = ...
+novy_record = SeqRecord(novy_uryvek,id="novy_uryvek228")
 
 # TODO 5: ulozte novy_record do souboru "streptococcus_50nt.fasta"
-...
+SeqIO.write(novy_uryvek, record, format="fasta")
